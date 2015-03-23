d6f4ac89ffc254b9af678ff2bf39f3d4b425a0e1
Vecksagos <bsb@ic.ufal.br>
2015-03-23 09:25:39 -0300

    Add SECTION on test

--- src/test.cpp
+++ src/test.cpp
@@ -9,15 +9,18 @@
 using namespace std;
 
 TEST_CASE( "Reader tests", "[read]") {
-    REQUIRE_THROWS(read("NoExiste"));
+    SECTION("Trying to read a no existent file")
+        REQUIRE_THROWS(read("NoExiste"));
     {
         ofstream stream(arquivo);
         if ((stream.rdstate() & ios_base::failbit))
             throw runtime_error("[TEST] Cannot create the file!");
 
-        //REQUIRE_NOTHROW(read(arquivo));
-        //map<uint8_t, unsigned int> empty;
-        //REQUIRE(read(arquivo) == empty);
+        SECTION("See if a empty file produces a empty map") {
+            REQUIRE_NOTHROW(read(arquivo));
+            map<uint8_t, unsigned int> empty;
+            REQUIRE(read(arquivo) == empty);
+        }
 
         char buffer[] = { '\x02', 'a', 'b', 'a', 'c', 'b' };
         stream.write(buffer, sizeof(buffer));
@@ -25,12 +28,14 @@ TEST_CASE( "Reader tests", "[read]") {
         stream.flush();
     }
 
-    auto freq = read(arquivo);
-    decltype(freq) hell {
-        {'\x02', 1},
-        {'a', 2},
-        {'b', 2},
-        {'c', 1}
-    };
-    REQUIRE(freq == hell);
+    SECTION("See if a fill file produces the right map") {
+        auto freq = read(arquivo);
+        decltype(freq) hell {
+            {'\x02', 1},
+                {'a', 2},
+                    {'b', 2},
+                        {'c', 1}
+        };
+        REQUIRE(freq == hell);
+    }
 }
